4475aac245030dc905ace7b2f032ecd47afea880
Benji Hooper <benji@sixp.in>
2016-07-20 15:39:01 +0100

    Updated the formula to use Hoverfly v0.7.1

--- hoverfly.rb
+++ hoverfly.rb
@@ -1,9 +1,9 @@
 class Hoverfly < Formula
   desc "lightweight & fantastic service virtualization/API simulation tool, aims to be developer/tester friendly"
   homepage "http://hoverfly.io"
-  url "https://github.com/SpectoLabs/hoverfly/archive/v0.7.0.tar.gz"
-  version "0.7.0"
-  sha256 "eec6b7a2e65a40021b3af2056519f5588757394ae82eb05caf2b73bb446172c0"
+  url "https://github.com/SpectoLabs/hoverfly/archive/v0.7.1.tar.gz"
+  version "0.7.1"
+  sha256 "4725070943d1803d48fefb146fe1564b0f6605a1d1d0c412383a117ec9a42210"
 
   depends_on "go" => :build
   depends_on "glide" => :build
@@ -15,9 +15,9 @@ class Hoverfly < Formula
     ENV["GOPATH"] = gopath
     ENV.prepend_create_path "PATH", gopath/"bin"
     cd gopath/"src/github.com/SpectoLabs/hoverfly" do
-	system "make", "hoverfly-build"
+	system "make", "hoverfly-build", "GIT_TAG_NAME=v0.7.1"
 	bin.install "target/hoverfly"
-	system "make", "hoverctl-build"
+	system "make", "hoverctl-build", "GIT_TAG_NAME=v0.7.1"
 	bin.install "target/hoverctl"
     end
   end
