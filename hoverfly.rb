9db0da03e7b01556e3086ea3afff9e998c94682f
Benji Hooper <benji@sixp.in>
2018-08-08 18:57:49 +0100

    Updating to v0.17.3

--- hoverfly.rb
+++ hoverfly.rb
@@ -1,9 +1,9 @@
 class Hoverfly < Formula
   desc "lightweight & fantastic service virtualization/API simulation tool, aims to be developer/tester friendly"
   homepage "http://hoverfly.io"
-  url "https://github.com/SpectoLabs/hoverfly/archive/v0.17.2.tar.gz"
-  version "0.17.2"
-  sha256 "8a128303230561975b4b7988a9c728bf2b223937e0612e8fd997bfc8ccb5b70c"
+  url "https://github.com/SpectoLabs/hoverfly/archive/v0.17.3.tar.gz"
+  version "0.17.3"
+  sha256 "d468e9b425004ef6992c532f6f02797e0f55697683e5f0f1e4749255453a79a4"
 
   depends_on "go" => :build
 
@@ -14,7 +14,7 @@ class Hoverfly < Formula
     ENV["GOPATH"] = gopath
     ENV.prepend_create_path "PATH", gopath/"bin"
     cd gopath/"src/github.com/SpectoLabs/hoverfly" do
-	system "make", "build", "GIT_TAG_NAME=v0.17.2"
+	system "make", "build", "GIT_TAG_NAME=v0.17.3"
 	bin.install "target/hoverfly"
 	bin.install "target/hoverctl"
     end
