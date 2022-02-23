4403e4f0ce8e58924ac8a70f13c628676892edea
Tommy Situ <tommy_situ@hotmail.com>
2022-02-23 22:53:16 +0000

    hoverfly 1.3.4

--- hoverfly.rb
+++ hoverfly.rb
@@ -1,8 +1,8 @@
 class Hoverfly < Formula
   desc "lightweight & fantastic service virtualization/API simulation tool, aims to be developer/tester friendly"
   homepage "http://hoverfly.io"
-  url "https://github.com/SpectoLabs/hoverfly/archive/v1.3.3.tar.gz"
-  sha256 "33ff8b2e564a5b657db0749f758b080a1ed827dc67033da3167b9dcf1f0cee94"
+  url "https://github.com/SpectoLabs/hoverfly/archive/v1.3.4.tar.gz"
+  sha256 "1d7764877a64795a5d2d7064b78666abe11ad6cc0cef3cd5ca5a654f55afc02e"
 
   depends_on "go" => :build
 
